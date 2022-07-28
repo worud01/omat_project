d3eb2cc6da20f260bc4fddd508fe53265506dfe1
MokuMokuSan7 <mms3592@gmail.com>
2022-07-28 23:45:23 +0900

    asd

--- omat_project/src/main/java/controller/MemberFrontController.java
+++ omat_project/src/main/java/controller/MemberFrontController.java
@@ -1,20 +1,13 @@
 package controller;
 
-import java.io.IOException;
-import java.util.Iterator;
+import java.io.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 
-import action.Action;
-import action.MemberJoinProAction;
-import action.MemberListAction;
-import action.MemberLoginProAction;
-import action.MemberLogoutAction;
-import vo.ActionForward;
+import action.*;
+import vo.*;
 @WebServlet("*.me")
 public class MemberFrontController extends HttpServlet {
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
@@ -25,66 +18,7 @@ public class MemberFrontController extends HttpServlet {
 		System.out.println(command);
 		ActionForward forward = null;
 		Action action = null;
-		if (command.equals("/MemberJoinFrom.me")) {
-			forward = new ActionForward();
-			forward.setPath("/member/member_join.jsp");
-			forward.setRedirect(false);
-			
-		} else if (command.equals("/MemberJoinPro.me")) {
-			try {
-				action =new MemberJoinProAction();
-				forward=action.execute(request, response);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-		}else if (command.equals("/MemberLoginFrom.me")) {
-		
-			forward = new ActionForward();
-			forward.setPath("/member/member_login.jsp");
-			forward.setRedirect(false);
-			
-		}else if (command.equals("/MemberLoginPro.me")) {
-		
-			try {
-				action=new MemberLoginProAction();
-				forward=action.execute(request, response);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			
-		}else if (command.equals("/MemberLogout.me")) {
-			try {
-				action=new MemberListAction();
-				forward=action.execute(request, response);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			
-		}else if (command.equals("/MemberList.me")) {
-			try {
-				action=new MemberLogoutAction();
-				forward=action.execute(request, response);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			
-		}else if (command.equals("/MemberLoginCgeck.me")) {
-			forward = new ActionForward();
-			forward.setPath("/member/member_check_id.jsp");
-			forward.setRedirect(false);
-		
-			
-		}
-		
+				
 		if (forward != null) {
 			if (forward.isRedirect()) {
 				response.sendRedirect(forward.getPath());
